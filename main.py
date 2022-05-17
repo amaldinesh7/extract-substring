df0687d10a506932af543198472758fe356fc58f
amaldinesh7 <amalkdinesh@gmail.com>
2022-05-17 12:18:39 +0530

    Initial commit

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    content = os.environ["content"]
+    delimiterOne = os.environ["delimiterOne"]
+    delimiterTwo = os.environ["delimiterTwo"]
 
-    my_output = f"Hello {my_input}"
+    output = content.partition('### **Description**')[2].partition('### **Checklist**')[0].strip()
 
-    print(f"::set-output name=myOutput::{my_output}")
+    my_output = f"{output}"
+
+    print(f"::set-output name=match::{my_output}")
 
 
 if __name__ == "__main__":
